46b230977044c1883c97fa53d64b5c5f3110b0ad
JeHun <jerry7041256@gmail.com>
2022-03-21 16:14:06 +0900

    feat:[front] 개발환경 세팅   S06P22B106- 프론트 개발환경 초기 세팅

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,31 +1,62 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-
-
-
-
-
-
-Vue.use(VueRouter)
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+import badge from "../views/badge/badgeMain.vue";
+import bloodCard from "../views/bloodCard/bloodCardCreate.vue";
+import daboWallet from "../views/daboWallet/daboWalletMain.vue";
+import donationBoard from "../views/donationBoard/donationBoardCreate.vue";
+import ranking from "../views/ranking/rankingMain.vue";
+import reservation from "../views/reservation/reservationMain.vue";
+import user from "../views/user/signUp.vue";
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: "/",
+    name: "home",
+    component: Home,
   },
   {
-    path: '/about',
-    name: 'about',
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
-]
+    path: "/",
+    name: "user",
+    component: user,
+  },
+  {
+    path: "/",
+    name: "reservation",
+    component: reservation,
+  },
+  {
+    path: "/",
+    name: "ranking",
+    component: ranking,
+  },
+  {
+    path: "/",
+    name: "donationBoard",
+    component: donationBoard,
+  },
+  {
+    path: "/",
+    name: "daboWallet",
+    component: daboWallet,
+  },
+  {
+    path: "/",
+    name: "bloodCard",
+    component: bloodCard,
+  },
+  {
+    path: "/",
+    name: "badge",
+    component: badge,
+  },
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
