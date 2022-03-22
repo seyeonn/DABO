c535cb23949fe59b8e31e4b2b3f9f34648f83ab5
seyeonn <ansse17@gmail.com>
2022-03-23 01:35:28 +0900

    feat : [front] 캠페인 목록 페이지 S06P22B106-236

---
+++ frontend/src/components/campaign/donationBoardCreate.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>donation
+    <form action="">
+      <h5>이미지 등록하기</h5>
+      <input multiple ref="image" type="file">
+
+      <h5>제목</h5>
+      <input type="text" name="" id="" placeholder="캠페인 제목">
+
+      <h5>내용</h5>
+      <input type="text" name="" id="" placeholder="캠페인 설명">
+
+      <h5>목표 갯수</h5>
+      <input type="text" name="" id="" placeholder="목표하는 갯수">
+
+      <h5>마감 기한</h5>
+      <input type="date" name="" id="">
+
+      <input type="submit" value="캠페인 등록">
+    </form>
+  </div>
+
+</template>
+
+<script>
+export default {
+};
+</script>
+
+<style></style>
---
+++ frontend/src/components/campaign/donationBoardDetail.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <img src="" alt="">
+        <h4>캠페인 제목</h4>
+        <p>캠페인 설명</p>
+        <div>달성도</div>
+        <p>n명의 후원자가 있습니다.</p>
+
+        <!-- 댓글 -->
+        
+        <button>헌혈증 기부하기</button>
+        <button>DABO로 후원하기</button>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="">
+    
+</style>
