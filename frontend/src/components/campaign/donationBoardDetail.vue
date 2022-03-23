6372676af9b01cc9d21df08d2c14ef3ab4f19009
seyeonn <ansse17@gmail.com>
2022-03-23 22:50:11 +0900

    feat : [front] 기부 페이지 레이아웃 구성 S06P22B106-246

--- frontend/src/components/campaign/donationBoardDetail.vue
+++ frontend/src/components/campaign/donationBoardDetail.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="detailBoard-page">
         <div>
-            <img src="https://i.imgur.com/httN9Bm.png" class="campaign-img" alt="">
+            <img src="https://i.imgur.com/httN9Bm.png" class="campaign-detail-img" alt="">
             <h4>캠페인 제목</h4>
             <p>캠페인 설명</p>
             <div>
@@ -83,7 +83,7 @@ export default {
   font-weight: 3px;
   margin-right: 10px;
 }
-.campaign-img {
+.campaign-detail-img {
   width: 100%;
   height: 200px;
 }
---
+++ frontend/src/components/campaign/myDonationDetail.vue
@@ -0,0 +1,211 @@
+<template>
+    <div class="myDonation-page">
+        <h4>나의 헌혈증</h4>
+        <p>총 n개 보유중</p>
+
+        <div class="donationList">
+            <p>기부할 헌혈증을 선택해주세요.</p>
+            <div>
+                <table class="dTable">
+                    <tr>
+                        <th></th>
+                        <th>증서 번호</th>
+                        <th>헌혈 일자</th>
+                        <th>혈핵원명</th>
+                    </tr>
+                    <tr>
+                        <td>
+                            <input type="checkbox" name="" id="">
+                        </td>
+                        <td>18-15963</td>
+                        <td>21.03.22</td>
+                        <td>SSAFY 헌혈의 집</td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <input type="checkbox" name="" id="">
+                        </td>
+                        <td>18-15922</td>
+                        <td>21.03.02</td>
+                        <td>SSAFY 헌혈의 집</td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+
+        <div>
+        <a href="#bDonation">
+        <button class="btn_red_donation" @click="bloodDonation">
+            <span>헌혈증 기부하기</span>
+        </button>
+        </a>
+        </div>
+        <div id="bDonation" class="modal-window">
+            <div>
+                    <p>총 n개의 헌혈증을 SSAFY 보건소로 전달합니다.</p>
+                    <sub>
+                        <b-icon icon="exclamation-circle" style="width: 10px; height: 10px;"></b-icon>
+                        헌혈증은 일정 시간의 대기 시간을 가진 뒤 자동으로 전달됩니다.
+                        전달 완료 후에는 취소하실 수 없으며, 관련 법령이 정하는 바에 따라 기부가 취소될 수 있습니다.
+                    </sub>
+                    <div>
+                    <a href="#">
+                        <button class="btn_red_cancel">
+                            <span>취소하기</span>
+                        </button>
+                    </a>
+                    <button class="btn_red_modal">
+                        <span>기부하기</span>
+                    </button>
+                    </div>
+            </div>
+            
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+        bloodDonation: function() {
+
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+.myDonation-page {
+  margin: 0 auto;
+  width: 90%;
+}
+.donationList {
+    border-radius: 5px;
+    box-shadow: 2px 2px 2px 2px rgb(190, 190, 190);
+}
+.dTable {
+    width: 100%;
+    text-align: center;
+}
+.dTable th:first-child {
+    width: 30px;
+}
+.dTable td:first-child{
+    text-align: center;
+}
+.btn_red_donation {
+  background-color: #e52d27;
+  color: #fff;
+  border-radius: 10px;
+  border: none;
+  width: 45%;
+  height: 35px;
+  font-size: 12px;
+  float: right;
+  margin-top: 15px;
+}
+.btn_red_modal {
+  background-color: #e52d27;
+  color: #fff;
+  border-radius: 10px;
+  border: none;
+  width: 120px;
+  height: 30px;
+  font-size: 12px;
+  float: right;
+  margin-top: 15px;
+}
+.btn_red_cancel {
+  background-color: #a3a3a3;
+  color: #fff;
+  border-radius: 10px;
+  border: none;
+  width: 120px;
+  height: 30px;
+  font-size: 12px;
+  float: right;
+  margin-top: 15px;
+  margin-left: 5px;
+}
+
+.modal-window {
+    position: fixed;
+    background-color: rgba(15, 15, 15, 0.25);
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    z-index: 999;
+    visibility: hidden;
+    opacity: 0;
+    pointer-events: none;
+    transition: all 0.3s;
+    &:target {
+      visibility: visible;
+      opacity: 1;
+      pointer-events: auto;
+    }
+    & > div {
+      width: 350px;
+      height: 170px;
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      background-color: #fff;
+      background-size: cover;
+      padding: 7px;
+    }
+    header {
+      font-weight: bold;
+    }
+    h1 {
+      font-size: 150%;
+      margin: 0 0 15px;
+    }
+  }
+  
+  .modal-close {
+    color: black;
+    line-height: 50px;
+    font-size: 100%;
+    position: absolute;
+    right: 10%;
+    text-align: center;
+    top: 10%;
+    width: 70px;
+    text-decoration: none;
+    &:hover {
+      color: black;
+    }
+  }
+
+  /* Demo Styles */
+  
+  
+  
+  a {
+    color: inherit;
+  }
+  
+  .modal-window {
+    & > div {
+      border-radius: 1rem;
+    }
+  }
+  
+  .modal-window div:not(:last-of-type) {
+    margin-bottom: 15px;
+  }
+  
+  small {
+    color: lightgray;
+  }
+  
+  
+</style>
