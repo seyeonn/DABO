ef10a748a93470348473df73b551b659114d9d07
jibin Jang <jb9992@daum.net>
2022-03-28 09:27:04 +0900

    feat: [front] footer / MyDABO 페이지 구현, Landing 구현 S06P22B106- footer[routing 설정은 페이지 완성 후 넣을 예정] MyDABO[페이지 구현 완료, 기능은 백엔드 개발 후 연동 예정] Landing[완료, 랜딩 애니메이션 후 4초 뒤 login 페이지로 이동| home routing을 landing으로 할 지 논의 필요]

---
+++ frontend/src/views/landing/landingPage.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <landing/>
+  </div>
+</template>
+
+<script>
+import landing from '@/components/landing/landingPage'
+
+export default {
+  data() {
+    return {
+
+    }
+  },
+  components: {
+    landing,
+  }
+}
+</script>
+
+<style>
+
+</style>
