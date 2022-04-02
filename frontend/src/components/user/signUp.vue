2396e61efd788b36561796d43cd7b39ba7fac921
손모은 <mo_ah@naver.com>
2022-04-02 20:39:56 +0900

    fix : [front] update blockchain config file

--- frontend/src/components/user/signUp.vue
+++ frontend/src/components/user/signUp.vue
@@ -83,6 +83,11 @@
         <button type="submit" class="btn_red">
           <span>SignUp</span>
         </button>
+        <button v-on:click="createWallet">지갑주소 생성하기</button>
+        <p>{{ privateKey }}</p>
+        <p>{{ walletAddress }}</p>
+
+        <button v-on:click="saveWallet">서버에 저장</button>
       </div>
     </form>
   </div>
@@ -91,8 +96,8 @@
 <script>
 import axios from "axios";
 
-/*import { registerWallet } from "@/api/wallet.js";
-import Web3 from "web3";*/
+import { registerWallet } from "@/api/wallet.js";
+import Web3 from "web3";
 
 export default {
   data() {
@@ -103,6 +108,9 @@ export default {
       email: "",
       password: "",
       passwordConfirm: "",
+      privateKey: "",
+      walletAddress: "",
+      userId : 1,
     };
   },
   methods: {
@@ -119,10 +127,10 @@ export default {
       console.log(userData);
 
       const response = await axios
-        .post("http://localhost:9090/api/user/signUp", userData)
+        .post("http://localhost:8080/api/user/signUp", userData)
         .then((res) => {
           console.log(res);
-          this.$router.push("login");
+          // this.$router.push("login");
         });
       console.log(response);
     },
@@ -133,11 +141,11 @@ export default {
        * 
        */
       
-      /*var web3 = new Web3();
+      var web3 = new Web3();
       var wallet = web3.eth.accounts.create("DABO_WALLET");
       this.walletAddress = wallet.address;
       this.privateKey = wallet.privateKey;
-      */
+      
     },
 
 
@@ -146,7 +154,7 @@ export default {
        * 생성된 사용자의 지갑 정보(주소, private key) 를 서버에 등록한다.
        */
 
-      /*
+      
       var vm = this;
 
       registerWallet(this.userId, this.walletAddress, function(res) {
@@ -154,9 +162,9 @@ export default {
         vm.$store.commit("setWalletAddress", res.data.address);
         alert("지갑 주소가 등록되었습니다.");
 
-        // vm.$router.push("/mypage/wallet_info");
+        vm.$router.push("login");
       });
-      */
+      
     }
   },
 };
