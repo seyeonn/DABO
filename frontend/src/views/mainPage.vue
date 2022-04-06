ce52799dcf33b9125e8ced6ad10ddef08c0955c8
JeHun <jerry7041256@gmail.com>
2022-04-06 19:02:39 +0900

    feat: [front] 메인 캠페인 푸터 오류 수정 S06P22B106- 닉네임 길이 메인 헤더 헌혈증 1개 입니다 사이즈 캠페인 css 리스트 css footer

--- frontend/src/views/mainPage.vue
+++ frontend/src/views/mainPage.vue
@@ -25,7 +25,7 @@
           </div>
         </div>
         <div class="contents-bloodcard d-flex">
-          <div><img src="@/assets/imoge.png" style="width: 80px;"/></div>
+          <div><img src="@/assets/imoge.png" style="width: 80px" /></div>
           <div>
             <p>
               나의 헌혈증은 {{ bloodcardCnt }}개 입니다.
@@ -247,7 +247,7 @@ export default {
 .main-greeting {
   margin-right: 15%;
   font-weight: bold;
-  font-size: 30px;
+  font-size: x-large;
   text-align: center;
 }
 .btn-profile button {
@@ -292,7 +292,7 @@ export default {
   width: 30px;
 }
 .contents-bloodcard {
-  height: 20vh;
+  height: 14vh;
   margin-top: 3%;
   margin-left: 5%;
   margin-right: 5%;
