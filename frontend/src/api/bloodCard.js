d13c2fd0097f9529a7d0eba5bf2a7f4e0f5bd71d
JeHun <jerry7041256@gmail.com>
2022-04-06 13:57:58 +0900

    feat : [front] 일부 수정   S06P22B106-

--- frontend/src/api/bloodCard.js
+++ frontend/src/api/bloodCard.js
@@ -2,44 +2,47 @@ import { tokenInstance } from "./tokenIndex.js";
 
 const instance = tokenInstance();
 
-function findByBloodCard(success, fail){
-    instance
+function findByBloodCard(success, fail) {
+  instance
     .get("api/bc/search")
-    .then(function(response){
-        success(response);
+    .then(function (response) {
+      success(response);
     })
     .catch(fail);
 }
 
-function bloodCardChageState(bloodCardId, transactionCardMessage, transactionCardToId, success, fail){
-    const body = {
-        bloodCardId: bloodCardId,
-        transactionCardMessage: transactionCardMessage,
-        transactionCardToId: transactionCardToId
-      };
-    instance
+function bloodCardChageState(
+  bloodCardId,
+  transactionCardMessage,
+  transactionCardToId,
+  success,
+  fail
+) {
+  const body = {
+    bloodCardId: bloodCardId,
+    transactionCardMessage: transactionCardMessage,
+    transactionCardToId: transactionCardToId,
+  };
+  instance
     .post("api/bc/donation", JSON.stringify(body))
     .then(success)
     .catch(fail);
 }
 
-function createBloodCard(body, success, fail){
-    instance
+function createBloodCard(body, success, fail) {
+  instance
     .post("api/bc/create", JSON.stringify(body))
     .then(success)
     .catch(fail);
 }
 
-function thSendBloodCard(success, fail){
-    instance
-    .get("api/bc/transactionHistory/send")
-    .then(success)
-    .catch(fail);
+function thSendBloodCard(success, fail) {
+  instance.get("api/bc/transactionHistory/send").then(success).catch(fail);
 }
 
-export{
-    findByBloodCard,
-    bloodCardChageState,
-    createBloodCard,
-    thSendBloodCard
-}
+export {
+  findByBloodCard,
+  bloodCardChageState,
+  createBloodCard,
+  thSendBloodCard,
+};
