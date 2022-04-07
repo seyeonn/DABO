ab82a35346a74f8ac5df315fbac32b5334969c27
JeHun <jerry7041256@gmail.com>
2022-04-07 23:02:26 +0900

    feat : [front] main campaign css 수정  S06P22B106- 메인 캠페인 css 수정

--- frontend/src/components/campaign/donationBoardListItem.vue
+++ frontend/src/components/campaign/donationBoardListItem.vue
@@ -4,19 +4,19 @@
       <img :src="this.baseURL + mediaUrl" alt="" />
     </div>
     <div class="">
-      <h4>{{ title }}</h4>
+      <h4 class="donation-title">{{ title }}</h4>
       <p>{{ content }}</p>
       <div>
         <div class="su-campaign small font-weight-bold d-flex">
           <div>
-            <span class="ratio">20%</span>
+            <span class="ratio">{{ process }}</span>
             <span
               class="float-right"
               style="margin-left: 5px; margin-right: 60px"
-              >5명</span
-            >
+              >헌혈증 {{ receiveBloodCard }} / {{ amount }}
+            </span>
           </div>
-          <span class="float-right">15일 남음</span>
+          <span class="float-right">{{ dueDate }}일 남음</span>
         </div>
         <div class="progress mb-2">
           <div
@@ -25,7 +25,10 @@
             aria-valuenow="20"
             aria-valuemin="0"
             aria-valuemax="100"
-          ></div>
+            :style="{ width: progress }"
+          >
+            {{ progress }}
+          </div>
         </div>
       </div>
     </div>
@@ -42,16 +45,37 @@ export default {
     title: String,
     content: String,
     mediaUrl: String,
-    receiveDabo: Number
+    receiveBloodCard: Number,
+    receiveDabo: Number,
+    amount: Number,
+    target: String,
+    deadLine: String,
   },
   data() {
     return {
       campaign: [],
       baseURL: API_BASE_URL,
+      progress: "",
+      dueDate: "",
     };
   },
   created() {
-    console.log(this.campaign);
+    console.log(this.amount);
+    this.progress =
+      Math.round(
+        (0.5 * (this.receiveBloodCard / this.amount) +
+          0.5 * (this.receiveDabo / this.target)) *
+          100 *
+          100
+      ) /
+        100 +
+      "%";
+    const strDate = this.deadLine.split("-");
+    const date = new Date(strDate[0], strDate[1] - 1, strDate[2]);
+    const today = new Date();
+    this.dueDate = Math.ceil((date - today) / (1000 * 60 * 60 * 24));
+
+    console.log(this.progress);
   },
   methods: {
     goDetail() {
@@ -65,8 +89,16 @@ export default {
 </script>
 
 <style scoped>
+.thumbnail-campaign img {
+  margin-top: 2%;
+  margin-bottom: 5%;
+}
+.donation-title {
+  font-weight: bold;
+}
 .progress-bar {
   width: 20%;
+  background-color: #f06464 !important;
 }
 .su-campaign {
   justify-content: space-between;
